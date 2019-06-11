a9c6c6bee27025e3d4ff2ba20c2ac15d8b5b2e58
whitelord <1091285927@qq.com>
2019-06-11 17:29:51 +0800

    2019/6/11 +原创微博生成csv表格

--- weibo/weibo_spider.py
+++ weibo/weibo_spider.py
@@ -6,12 +6,13 @@ import requests
 from pyquery import PyQuery as pq
 
 import json
+import csv
 
 
 base_url = 'https://m.weibo.cn/api/container/getIndex?'
 
 headers = {
-    'Referer' : 'https://m.weibo.cn/p/2304132611891653_-_WEIBO_SECOND_PROFILE_WEIBO',
+    'Referer' : 'https://m.weibo.cn/u/2162509662',
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
     'X-Requested-With' : 'XMLHttpRequest'
 }
@@ -19,7 +20,7 @@ headers = {
 # 获取函数 -> json
 def get_page(page):
     parmas = {
-        'containerid' : '2304132611891653',
+        'containerid' : '1076032162509662',
         'page_type' : '03',
         'page' : page
     }
@@ -42,22 +43,22 @@ def parse_page(json):
             weibo['id'] = item.get('id')
             weibo['text'] = pq(item.get('text')).text()
             weibo['attitudes'] = item.get('attitudes_count')
-            weibo['comments'] = item.get('comment_count')
+            weibo['comments'] = item.get('comments_count')
             weibo['reports'] = item.get('reports_count')
             weibo['retweeted'] = item.get('retweeted_status')
             yield weibo
 
 if __name__ == "__main__":
-    count = 0
+    count = 0  
     for page in range(1,11):
         json = get_page(page)
         results = parse_page(json)
         for result in results:
-            if '陈霸彪' in result.get('text'):
-                count += 1
-            if result.get('retweeted'):
-                retweet = 1
-            else:
-                print(result)
-        retweet = 0
-    print('你在前10页原创微博里提到了@都市丽人陈霸彪'+ str(count) +'次！\n')
+            # 返回原创微博 即retweeted值为none
+            if result.get('retweeted') == None:
+                with open('data.csv','a', encoding= 'utf-8') as csvfile:
+                    fieldnames = ['id','text','attitudes','comments','reports','retweeted']
+                    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+                    writer.writeheader()
+                    writer.writerow(result)
+
